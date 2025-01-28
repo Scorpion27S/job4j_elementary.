b106160028c03b3b7efa4c38fe4eb4543a45abb7
Scorpion27S <Koklyuhin-gleb@mail.ru>
2025-01-28 21:44:08 +0700

    6.0.3. Заполнение массива. [#226300 #442725]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,12 +1,20 @@
 package ru.job4j.array;
 
-import com.sun.source.util.SourcePositions;
-
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Gleb";
+        names[1] = "Anton-Lol";
+        names[2] = "Lika";
+        names[3] = "Tanki";
+        System.out.println("Массив names: " + names.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
