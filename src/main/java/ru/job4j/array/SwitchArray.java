4b38d255ec7051a4e9e09c5dd59dd82fb65a0132
Scorpion27S <Koklyuhin-gleb@mail.ru>
2025-01-31 22:23:40 +0700

    изменения в SwitchArray

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -16,10 +16,10 @@ public class SwitchArray {
     }
 
     public static void main(String[] args) {
-        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7 , 8};
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
         int[] result = swapBorder(numbers);
-        for (int index = 0; index < result.length; index++) {
-            System.out.println(result[index]);
+        for (int i : result) {
+            System.out.println(i);
         }
     }
 }
